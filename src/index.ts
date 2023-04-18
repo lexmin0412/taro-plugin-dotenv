e20090b0e93616cc058aeebe47ae64dc288a3eab
lexmin0412 <zhangle_dev@outlook.com>
2023-04-18 19:49:53 +0800

    optimize: 兼容.env环境配置为空的情况

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
 const dotenv = require('dotenv')
 const env = dotenv.config().parsed
-const envKeys = Object.keys(env).reduce((prev, next) => {
-	prev[`process.env.${next}`] = JSON.stringify(env[next])
-	return prev
-}, {})
 
 export default (ctx: any) => {
   const {
@@ -14,6 +10,17 @@ export default (ctx: any) => {
 
 	ctx.modifyWebpackChain((args: { chain: any, webpack: any })=>{
 		console.log(chalk.yellow('插件 '), 'taro-plugin-dotenv 读取 .env 文件并注入 webpack 配置')
+
+		if (typeof env === 'undefined') {
+			console.log(chalk.blueBright('结束 '), '.env 文件环境配置为空')
+			console.log();
+			return
+		}
+
+		const envKeys = Object.keys(env).reduce((prev, next) => {
+			prev[`process.env.${next}`] = JSON.stringify(env[next])
+			return prev
+		}, {})
 		console.log(chalk.greenBright('注入 '), envKeys)
 		const { chain, webpack } = args
 		chain.merge({
